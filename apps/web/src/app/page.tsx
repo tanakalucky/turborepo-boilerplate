74938324a1fadc48e444689b168baf5b2b59a159
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-07-27 23:00:56 +0000

    fix workflow deploy service with terraform

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -35,6 +35,9 @@ const LINKS = [
 ];
 
 export default function Page(): JSX.Element {
+  // eslint-disable-next-line no-console -- test
+  console.log(process.env.GRAPHQL);
+
   return (
     <main className='flex flex-col items-center justify-between min-h-screen p-24'>
       <div className='z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex'>
