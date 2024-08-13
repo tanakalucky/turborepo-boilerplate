4bd064748518644903964fa7c025684ab8284d71
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-08-13 01:25:31 +0000

    refactor: Delete unnecessary output values

--- apps/api/terraform/outputs.tf
+++ apps/api/terraform/outputs.tf
@@ -1,12 +1,3 @@
-output "api_id" {
-  value = aws_appsync_graphql_api.this.id
-}
-
-output "api_key" {
-  sensitive = true
-  value     = aws_appsync_api_key.this.key
-}
-
 output "graphql_url" {
   value = aws_appsync_graphql_api.this.uris["GRAPHQL"]
 }
